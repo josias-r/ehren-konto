3dbec623bc7830c3242872aa712ebdc64f383165
josias-r <josias.ribi@gmail.com>
2023-05-15 19:50:21 +0200

    fix: add toast components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default async function RootLayout({
       </head>
       <body className={`${inter.className} bg-background dark h-full`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
