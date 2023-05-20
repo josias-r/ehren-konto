2090bf9a163c5ac711c2a5612d29c94320ff259f
josias-r <josias.ribi@gmail.com>
2023-05-20 18:36:04 +0200

    fix: refactor getAllGrops to kysely

--- app/@app/(with-profile)/groups/getAllGroupsForUser.ts
+++ app/@app/(with-profile)/groups/getAllGroupsForUser.ts
@@ -1,106 +1,103 @@
 import "server-only";
 
-import { prisma } from "@/lib/prisma-client";
+import { db } from "@/lib/kysely-client";
 
 async function getAllGroupsForUser(userId: string) {
-  const userWithGroups = await prisma.user.findFirst({
-    select: {
-      GroupMember: {
-        orderBy: {
-          Group: {
-            createdAt: "desc",
-          },
-        },
-        select: {
-          Group: {
-            select: {
-              _count: {
-                select: {
-                  GroupMembers: true,
-                  Activities: true,
-                },
-              },
-              groupId: true,
-              name: true,
-              description: true,
-              GroupMembers: {
-                orderBy: [
-                  {
-                    ehre: "desc",
-                  },
-                  {
-                    User: {
-                      name: "asc",
-                    },
-                  },
-                ],
-                select: {
-                  role: true,
-                  ehre: true,
-                  User: {
-                    select: {
-                      avatar: true,
-                      userId: true,
-                      name: true,
-                      nick: true,
-                    },
-                  },
-                },
-                take: 5,
-              },
-              Activities: {
-                orderBy: {
-                  from: "asc",
-                },
-                select: {
-                  activityId: true,
-                  name: true,
-                  from: true,
-                  to: true,
-                  emoji: true,
-                  color: true,
-                  ActivityParticipants: {
-                    orderBy: {
-                      createdAt: "desc",
-                    },
-                    select: {
-                      confirmed: true,
-                      User: {
-                        select: {
-                          userId: true,
-                          name: true,
-                        },
-                      },
-                    },
-                  },
-                },
-                take: 6,
-              },
-            },
-          },
-        },
-      },
-    },
-    where: {
-      userId: userId,
-    },
+  const groups = await db
+    .selectFrom("Group")
+    .where(({ exists, selectFrom }) =>
+      exists(
+        selectFrom("GroupMember")
+          .whereRef("GroupMember.groupId", "=", "Group.groupId")
+          .where("GroupMember.userId", "=", userId)
+      )
+    )
+    .select(["Group.groupId", "Group.name", "Group.description"])
+    .execute();
+
+  const userWithGroups = groups.map(async (group) => {
+    const groupMembersQuery = db
+      .selectFrom("GroupMember")
+      .where("GroupMember.groupId", "=", group.groupId)
+      .innerJoin("User", "User.userId", "GroupMember.userId");
+
+    const groupMembers = await groupMembersQuery
+      .select([
+        "GroupMember.role",
+        "GroupMember.ehre",
+        "User.userId",
+        "User.avatar",
+        "User.name",
+        "User.nick",
+      ])
+      .orderBy("GroupMember.ehre", "desc")
+      .orderBy("User.name", "asc")
+      .limit(5)
+      .execute();
+
+    const groupMembersCount = await groupMembersQuery
+      .select(db.fn.countAll<number>().as("count"))
+      .execute();
+
+    const acivitiesQuery = db
+      .selectFrom("Activity")
+      .where("Activity.groupId", "=", group.groupId);
+
+    const activities = await acivitiesQuery
+      .orderBy("Activity.from", "asc")
+      .select([
+        "Activity.activityId",
+        "Activity.name",
+        "Activity.from",
+        "Activity.to",
+        "Activity.emoji",
+        "Activity.color",
+      ])
+      .limit(6)
+      .execute();
+
+    const activitiesCount = await acivitiesQuery
+      .select(db.fn.countAll<number>().as("count"))
+      .execute();
+
+    console.log(groupMembersCount, activitiesCount);
+
+    const activitiesWithParticipants = await Promise.all(
+      activities.map(async (activity) => {
+        const participants = await db
+          .selectFrom("ActivityParticipant")
+          .where("ActivityParticipant.activityId", "=", activity.activityId)
+          .innerJoin("User", "User.userId", "ActivityParticipant.userId")
+          .select(["ActivityParticipant.confirmed", "User.userId", "User.name"])
+          .orderBy("ActivityParticipant.createdAt", "desc")
+          .execute();
+
+        return {
+          ...activity,
+          ActivityParticipants: participants,
+        };
+      })
+    );
+
+    const groupWithMembersAndActivities = {
+      ...group,
+      totalMembers: groupMembersCount[0].count,
+      totalActivities: activitiesCount[0].count,
+      GroupMembers: groupMembers,
+      Activities: activitiesWithParticipants,
+    };
+
+    return groupWithMembersAndActivities;
   });
 
-  return userWithGroups?.GroupMember.map((userGroupMembership) => ({
-    groupId: userGroupMembership.Group.groupId,
-    name: userGroupMembership.Group.name,
-    description: userGroupMembership.Group.description,
-    totalMembers: userGroupMembership.Group._count.GroupMembers,
-    members: userGroupMembership.Group.GroupMembers.map((groupMember) => ({
-      userId: groupMember.User.userId,
-      avatar: groupMember.User.avatar,
-      role: groupMember.role,
-      ehre: groupMember.ehre,
-      name: groupMember.User.name,
-      nick: groupMember.User.nick,
-    })),
-    totalActivities: userGroupMembership.Group._count.Activities,
-    activities: userGroupMembership.Group.Activities.map((activity) => ({
+  return (await Promise.all(userWithGroups)).map((userGroupMembership) => ({
+    groupId: userGroupMembership.groupId,
+    name: userGroupMembership.name,
+    description: userGroupMembership.description,
+    totalMembers: userGroupMembership.totalMembers,
+    members: userGroupMembership.GroupMembers,
+    totalActivities: userGroupMembership.totalActivities,
+    activities: userGroupMembership.Activities.map((activity) => ({
       activityId: activity.activityId,
       name: activity.name,
       from: activity.from,
@@ -108,9 +105,9 @@ async function getAllGroupsForUser(userId: string) {
       emoji: activity.emoji,
       color: activity.color,
       participants: activity.ActivityParticipants.map((participant) => ({
-        userId: participant.User.userId,
+        userId: participant.userId,
         confirmed: participant.confirmed,
-        name: participant.User.name,
+        name: participant.name,
       })),
     })),
   }));
