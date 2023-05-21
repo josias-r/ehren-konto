b2b6483f1baaa31e51b093777d1ef49f257396ee
josias-r <josias.ribi@gmail.com>
2023-05-21 23:05:57 +0200

    no push permissions request for now

--- components/ServiceWorkerProvider.tsx
+++ components/ServiceWorkerProvider.tsx
@@ -11,11 +11,11 @@ function ServiceWorkerProvider({ children }: ServiceWorkerProviderProps) {
     const asyncWrapper = async () => {
       if ("serviceWorker" in navigator) {
         const registration = await navigator.serviceWorker.register("/sw.js");
-        const pushSubscription = await registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey:
-            "BFeJlZifA8Q71AKiTezmx77i_bRVPTJIWsXSuFsS9BirWfAIG4O0nu_Mxmsyz6FTpalEjKZfwnnZJBSL1dKqMC4",
-        });
+        // const pushSubscription = await registration.pushManager.subscribe({
+        //   userVisibleOnly: true,
+        //   applicationServerKey:
+        //     "BFeJlZifA8Q71AKiTezmx77i_bRVPTJIWsXSuFsS9BirWfAIG4O0nu_Mxmsyz6FTpalEjKZfwnnZJBSL1dKqMC4",
+        // });
       }
     };
     asyncWrapper();
