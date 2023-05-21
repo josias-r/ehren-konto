4a8fca3f7d716ccd2ce74b7b0bc0c290dcad02fe
josias-r <josias.ribi@gmail.com>
2023-05-21 19:40:47 +0200

    ...

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
 import LoginForm from "@/app/(auth)/login/LoginForm";
 
+export const metadata = {
+  title: "Sign in",
+  description:
+    "Experience more with your friends by easily creating and participating in activities together!",
+};
+
 function Login() {
   return (
     <main className="p-4 h-full max-w-md mx-auto grid items-center">
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,11 @@
 import SignupForm from "@/app/(auth)/signup/SignupForm";
 
+export const metadata = {
+  title: "Sign up",
+  description:
+    "Experience more with your friends by easily creating and participating in activities together!",
+};
+
 function Signup() {
   return (
     <main className="p-4 h-full max-w-md mx-auto grid items-center">
