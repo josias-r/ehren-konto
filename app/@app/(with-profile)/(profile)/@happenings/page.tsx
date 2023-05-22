6e08b72244d0d03895aaa44db4f3b957a97aef96
josias-r <josias.ribi@gmail.com>
2023-05-22 11:57:59 +0200

    disable broken feature

--- app/@app/(with-profile)/(profile)/@happenings/page.tsx
+++ app/@app/(with-profile)/(profile)/@happenings/page.tsx
@@ -10,10 +10,11 @@ import { getUserId } from "@/app/(auth)/getUserId";
 import getRelevantHappenings from "@/app/@app/(with-profile)/(profile)/@happenings/getRelevantHappenings";
 import HappeningItem from "./HappeningItem";
 import { EmptyState } from "@/components/ui/empty-state";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
 
 async function Happenings() {
   const userId = getUserId();
-  const relevantHappenings = await getRelevantHappenings(userId);
   return (
     <Card className="mx-auto max-w-md">
       <CardHeader>
@@ -21,27 +22,45 @@ async function Happenings() {
         <CardDescription>What is happening?</CardDescription>
       </CardHeader>
       <CardContent>
-        {!!relevantHappenings.length && (
-          <div className="space-y-4">
-            {relevantHappenings.map((happening) => (
-              <HappeningItem
-                key={happening.happeningsId}
-                happening={happening}
-              />
-            ))}
-          </div>
-        )}
-        {!relevantHappenings.length && (
-          <div className="mt-6">
-            <EmptyState
-              title="Empty"
-              message="Nothing is happening right now"
-            />
-          </div>
-        )}
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>disabled</AlertTitle>
+          <AlertDescription>
+            Due to a bug this feature is currently disabled.
+          </AlertDescription>
+        </Alert>
       </CardContent>
     </Card>
   );
+  // const relevantHappenings = await getRelevantHappenings(userId);
+  // return (
+  //   <Card className="mx-auto max-w-md">
+  //     <CardHeader>
+  //       <CardTitle>Happenings</CardTitle>
+  //       <CardDescription>What is happening?</CardDescription>
+  //     </CardHeader>
+  //     <CardContent>
+  //       {!!relevantHappenings.length && (
+  //         <div className="space-y-4">
+  //           {relevantHappenings.map((happening) => (
+  //             <HappeningItem
+  //               key={happening.happeningsId}
+  //               happening={happening}
+  //             />
+  //           ))}
+  //         </div>
+  //       )}
+  //       {!relevantHappenings.length && (
+  //         <div className="mt-6">
+  //           <EmptyState
+  //             title="Empty"
+  //             message="Nothing is happening right now"
+  //           />
+  //         </div>
+  //       )}
+  //     </CardContent>
+  //   </Card>
+  // );
 }
 
 export default Happenings;
